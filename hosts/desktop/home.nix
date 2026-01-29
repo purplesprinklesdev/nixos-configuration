fc65812624fb51d586f19a1287217a5f7a62098e
purplesprinklesdev <ought-impale-doozy@duck.com>
2026-01-29 12:06:46 -0500

    Add yt-dlp - desktop-nixos Gen 171

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -236,6 +236,8 @@
     # Flipper Zero
     qFlipper
     
+    yt-dlp
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
