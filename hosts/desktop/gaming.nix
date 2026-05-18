d4815a96cd17ac2175555040a02d7ece1c16d813
purplesprinklesdev <ought-impale-doozy@duck.com>
2026-05-18 00:48:33 -0400

    steam controller

--- hosts/desktop/gaming.nix
+++ hosts/desktop/gaming.nix
@@ -4,7 +4,12 @@
   programs.steam = {
     enable = true;
     gamescopeSession.enable = true;
+    extest.enable = true;
+    extraPackages = with pkgs; [
+      hidapi
+    ];
   };
+  hardware.steam-hardware.enable = true;
   programs.gamemode.enable = true;
 
   environment.systemPackages = with pkgs; [
